8bbbbe809c22057a0caad16300634b32a63576dc
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-25 13:43:08 +0530

    instagram stories

--- App.js
+++ App.js
@@ -1,21 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/*import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  
+  authDomain: "firstreactproject-3a081.firebaseapp.com",
+  projectId: "firstreactproject-3a081",
+  storageBucket: "firstreactproject-3a081.appspot.com",
+  messagingSenderId: "787295746675",
+  appId: "1:787295746675:web:5a5b5518e331b013a50806"
+};
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+import LoginScreen from './components/auth/Login';
+
+const Stack = createStackNavigator();
+
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+
+        </View>
+
+      )
+
+    } if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Landing'>
+
+            <Stack.Screen name='Landing' component={LandingScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen name='Register' component={RegisterScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen name='Login' component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+
+      );
+    }
+    else {
+      return (
+
+        <View>
+          <Text>
+            user is loggedIn
+            </Text>
+        </View>
+      )
+    }
+
+
+
+  }
+}
+
+export default App*/
+
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import InstaStories from './components/instaStories/instaStories'
+
+function App() {
+  return (
+    <SafeAreaView>
+      <InstaStories />
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+
+
---
+++ components/instaStories/instaStories.jsx
@@ -0,0 +1,132 @@
+/*import React from 'react'
+import { ScrollView, Text } from 'react-native'
+
+function InstaStories() {
+    return (
+
+        <ScrollView style={{ flex: 1 }}>
+            <Text>
+                stories
+
+            </Text>
+        </ScrollView>
+    )
+}
+
+export default InstaStories*/
+
+import React from "react";
+import {
+    View,
+    SafeAreaView,
+    Text,
+    FlatList,
+    Image,
+    TouchableOpacity
+} from "react-native";
+
+import options from "../../assets/options.png";
+import like from "../../assets/like.png";
+import comment from "../../assets/comment.png";
+import send from "../../assets/send.png";
+import save from "../../assets/save.png";
+
+import styles from "./styles";
+
+export default function Feed() {
+    const posts = [
+        {
+            id: "1",
+            author: "_iurgan.w",
+            picture_url:
+                "https://observatoriodocinema.bol.uol.com.br/wp-content/uploads/2019/08/69047060_359538678332469_1243013082205126656_n.png",
+            likes: 1272,
+            description: "Saiu o filme do coringa!!!",
+            hashtags: "#cinema #joker",
+            place: "Cinema do Shopping"
+        },
+        {
+            id: "2",
+            author: "_iurgan.w",
+            picture_url:
+                "https://s2.glbimg.com/VUgvvuWb7QZ_q0Rfqg_EHLIV32U=/top/e.glbimg.com/og/ed/f/original/2019/08/05/angelina1.png",
+            likes: 784,
+            description: "Saiu o filme malevola!!!",
+            hashtags: "#cinema #malevola",
+            place: "Cinema do Shopping"
+        },
+        {
+            id: "3",
+            author: "_iurgan.w",
+            picture_url:
+                "http://br.web.img3.acsta.net/pictures/19/05/07/20/54/2901026.jpg",
+            likes: 397,
+            description: "Saiu o filme rei leão!!!",
+            hashtags: "#cinema #reileao",
+            place: "Cinema do Shopping"
+        }
+    ];
+
+    function renderItem({ item: post }) {
+        return (
+            <SafeAreaView style={styles.post}>
+                <View style={styles.postHeader}>
+                    <View style={styles.userInfo}>
+                        <Text style={styles.author}>{post.author}</Text>
+                        <Text style={styles.place}>{post.place}</Text>
+                    </View>
+
+                    <View style={styles.postOptions}>
+                        <TouchableOpacity>
+                            <Image source={options} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View>
+                    <Image style={styles.postImage} source={{ uri: post.picture_url }} />
+                </View>
+
+                <View style={styles.footer}>
+                    <View style={styles.actions}>
+                        <View style={styles.leftActions}>
+                            <TouchableOpacity style={styles.action}>
+                                <Image source={like} />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={styles.action}>
+                                <Image source={comment} />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <Image source={send} />
+                            </TouchableOpacity>
+                        </View>
+
+                        <View>
+                            <TouchableOpacity>
+                                <Image source={save} />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    <View>
+                        <Text style={styles.likes}>{post.likes} likes</Text>
+                        <Text style={styles.hashtags}>{post.hashtags}</Text>
+                        <Text style={styles.description}>{post.description}</Text>
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
+    }
+
+    return (
+        <View>
+            <FlatList
+                data={posts}
+                keyExtractor={item => item.id}
+                renderItem={renderItem}
+            />
+        </View>
+    );
+}
